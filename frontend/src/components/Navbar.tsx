eb823a331a2df819e99cc967471018cbfebfe356
Maksimova Snezhana <steph.klepa@gmail.com>
2025-11-04 23:59:09 +0300

    added: Navbar (isLoggedIn case)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import '../index.css'
-import { EnvironmentOutlined, HeartFilled } from '@ant-design/icons';
+import { EnvironmentOutlined, HeartFilled, MessageOutlined, UserOutlined } from '@ant-design/icons';
 
 function Navbar() {
+
+  const isLoggedIn = true;
 
   return (
     <nav className="flex items-center justify-between p-4 bg-white border-b">
@@ -12,14 +14,29 @@ function Navbar() {
             <span className="text-[1rem] pl-[0.05rem]">Москва</span>
             </div>
         </div>
-
-        <div className="flex items-center gap-4">
-            <HeartFilled style={{ fontSize: '20px', paddingRight: '1rem' }}/>
-            <div className="flex gap-3 text-sm">
-            <span className="text-[1rem] pr-[0.5rem] font-inter">Войти</span>
-            <span className="text-[1rem] pr-[1rem] mr-[2.5rem] font-inter">Зарегистрироваться</span>
+        
+        {
+          !isLoggedIn && 
+            <div className="flex items-center gap-4">
+              <HeartFilled style={{ fontSize: '20px', paddingRight: '1rem' }}/>
+              <div className="flex gap-3 text-sm">
+                <span className="text-[1rem] pr-[0.5rem] font-inter">Войти</span>
+                <span className="text-[1rem] pr-[1rem] mr-[2.5rem] font-inter">Зарегистрироваться</span>
+              </div>
             </div>
-        </div>
+        }
+
+        {
+          isLoggedIn && 
+            <div className="flex items-center gap-4">
+              <HeartFilled style={{ fontSize: '20px', paddingRight: '1rem' }}/>
+              <div className="flex gap-3 text-sm">
+                <MessageOutlined style={{ fontSize: '20px', paddingRight: '1rem' }}/>
+                <UserOutlined style={{ fontSize: '20px', paddingRight: '2.5rem' }}/>
+              </div>
+            </div>
+        }
+            
     </nav>
   )
 }
