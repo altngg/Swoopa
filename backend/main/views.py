4d9cf318e818c7e671b21febb3c2f6a290cb0cc7
Sunrise443 <sunkensun0@gmail.com>
2025-11-24 21:05:41 +0300

    added editing publication

--- backend/main/views.py
+++ backend/main/views.py
@@ -20,3 +20,25 @@ def create_publication(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def edit_publication(request, pk):
+    try:
+        publication = Publication.objects.get(pk=pk)
+    except Publication.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        publication.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = PublicationSerializer(publication, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
