d062517e5efe440c7ee7ac815d791b59a168ac8f
altngg <alevtina.gilim@yandex.ru>
2026-01-04 16:28:20 +0300

    Добавление моделей пользователя, чата и геолокации

--- backend/main/models.py
+++ backend/main/models.py
@@ -28,15 +28,18 @@ class Publication(models.Model):
     description = models.TextField(blank=True)
     main_image = models.ImageField(upload_to='publications/main/')
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=100) # сделать так, чтобы при создании карточки локация бралась из локации автора
+    location = models.CharField(max_length=255) # сделать так, чтобы при создании карточки локация бралась из локации автора
 
     publication_type = models.ForeignKey(PublicationType, on_delete=models.CASCADE, related_name='publications_type')
     status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='publications_status')
-    # author
+    
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publications_author')
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
+        if not self.location and self.author and self.author.location:
+            self.location = str(self.author.location)
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -46,3 +49,11 @@ class Publication(models.Model):
 class PublicationImage(models.Model):
     product = models.ForeignKey(Publication, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='products/extra/')
+
+class Geolocation(models.Model):
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.city}, {self.district}" if self.district else self.city
+    
