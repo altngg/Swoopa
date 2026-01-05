dde40a78601355bcbfd81cf29099c63ce241aa88
Sunrise443 <sunkensun0@gmail.com>
2026-01-05 17:30:13 +0300

    better serializer, fixed editing, added getting one puclication

--- backend/main/views.py
+++ backend/main/views.py
@@ -12,6 +12,19 @@ def get_publications(request):
     serializedData = PublicationSerializer(publications, many=True).data
     return Response(serializedData)
 
+@api_view(['GET'])
+def get_publication_by_slug(request, slug):
+    try:
+        publication = Publication.objects.get(slug=slug)
+    except Publication.DoesNotExist:
+        return Response(
+            {"error": "Publication not found."},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    serializer = PublicationSerializer(publication)
+    return Response(serializer.data)
+
 @api_view(['POST'])
 def create_publication(request):
     data = request.data
@@ -21,20 +34,28 @@ def create_publication(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT', 'DELETE'])
-def edit_publication(request, pk):
+@api_view(['PATCH', 'PUT', 'DELETE'])
+def edit_publication(request, slug):
     try:
-        publication = Publication.objects.get(pk=pk)
+        publication = Publication.objects.get(slug=slug)
     except Publication.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "Publication not found."},
+            status=status.HTTP_404_NOT_FOUND
+        )
     
     if request.method == 'DELETE':
         publication.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-    elif request.method == 'PUT':
-        data = request.data
-        serializer = PublicationSerializer(publication, data=data)
+    elif request.method in ['PUT', 'PATCH']:
+        partial = request.method == 'PATCH'
+        
+        serializer = PublicationSerializer(
+            publication, 
+            data=request.data, 
+            partial=partial,
+        )
 
         if serializer.is_valid():
             serializer.save()
