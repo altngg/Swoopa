e4cc3d74a9fe1a3ab058b3834b5273f9a71cb645
Sunrise443 <sunkensun0@gmail.com>
2026-01-10 16:01:30 +0300

    added axios into node modules, docker fixed and working (probably needs improvements), port changed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,8 +93,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5433'),
-        'ATOMIC_REQUESTS': True,
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
