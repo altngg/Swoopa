34eada5af3a908c0a6b622512c82beae7dfe564d
Sunrise443 <sunkensun0@gmail.com>
2026-01-10 16:17:34 +0300

    healthcheck added in dockerfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'db'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ATOMIC_REQUESTS': True,
     }
 }
 
